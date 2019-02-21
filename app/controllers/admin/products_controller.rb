221b49014ab290a1d9450b70cfe45a586ee7f59d
zengzheng <zengzheng2009@gmail.com>
2019-02-22 01:20:14 +0800

    add admin

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,8 +1,11 @@
 class Admin::ProductsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :admin_required
+
   def index
     @products = Product.all
   end
-  
+
   def new
     @product = Product.new
   end
