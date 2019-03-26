f27989a656d03bc5e2d197a20726bf292af60f4a
zengzheng <zengzheng2009@gmail.com>
2019-03-26 14:07:14 +0800

    fix bug

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -3,7 +3,7 @@ class CartItemsController < ApplicationController
 
   def destroy
     @cart = current_cart
-    @cart_item = @cart.cart_item.find_by(product_id: params[:id])
+    @cart_item = @cart.cart_items.find_by(product_id: params[:id])
     @product = @cart_item.product
     @cart_item.destroy
 
