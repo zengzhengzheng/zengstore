b72a4c3668c3e903964f02045498e6d2d0777659
zengzheng <zengzheng2009@gmail.com>
2019-03-26 23:32:11 +0800

    fix bug

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,5 +5,5 @@ class Order < ApplicationRecord
   validates :billing_name, presence: true
   validates :billing_address, presence: true
   validates :shipping_name, presence: true
-  validates :shippint_address, presence: true
+  validates :shipping_address, presence: true
 end
